b45453210ec0c4eddd03e6dc5ad1eeb4c84adc2c
Syed Murtaza Kazmi <syed.murtaza.kazmi@gmail.com>
2021-06-26 13:09:58 -0400

    correcting schema

--- schema.sql
+++ schema.sql
@@ -49,6 +49,5 @@ emp_no INT NOT NULL,
 title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
